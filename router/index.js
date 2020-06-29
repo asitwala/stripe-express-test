275139497f0ad6a386ff241bcc47cd800181203b
Amy Sitwala <asitwala17@gmail.com>
2020-06-29 09:41:43 -0700

    Updating flow to write to Google sheets

--- router/index.js
+++ router/index.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const querystring = require('querystring');
+
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+var stripe = require('stripe')(process.env.STRIPE_API_KEY);
+
+// spreadsheet key is the long id in the sheets URL
+const doc = new GoogleSpreadsheet('1A65pgQyXCGh0n8Alp2R9rJirEQb7jFThB7I_amXl6PY');
+
 // const STRIPE_API = require('../api/stripe-functions');
 var router = express.Router();
 
@@ -10,17 +17,31 @@ router.get('/', async (req, res) => {
 
 router.get('/onboarding/setup', async (req, res) => {
   // Test client ID: ca_HYI7b0d1kneMHXpkggxRtkwrX1f5EdlB
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+  });
 
-  // Reference: https://github.com/stripe/stripe-connect-rocketrides/blob/master/server/routes/pilots/stripe.js
-  // Generate a random string as `state` to protect from CSRF and include it in the session
-  req.session.state = Math.random()
-    .toString(36)
-    .slice(2);
+  await doc.loadInfo();
+
+  const sheet = doc.sheetsByIndex[0];
+  const newRow = await sheet.addRow({ FV_Name: req.query.vessel, FV_User_Email: req.query.email });
+
+  // // Reference: https://github.com/stripe/stripe-connect-rocketrides/blob/master/server/routes/pilots/stripe.js
+  // // Generate a random string as `state` to protect from CSRF and include it in the session
+  // req.session.state = Math.random()
+  //   .toString(36)
+  //   .slice(2);
+
+  req.session.state = req.query.email;
 
   let parameters = {
-    redirect_uri: 'https://connect.stripe.com/connect/default/oauth/test',
+    redirect_uri: 'http://localhost:3000/onboarding/complete',
     client_id: process.env.STRIPE_CLIENT_ID,
     state: req.session.state,
+    'stripe_user[business_type]': 'company',
+    'stripe_user[business_name]': req.query.vessel || undefined,
+    'stripe_user[email]': req.query.email || undefined,
   };
   res.redirect(
     'https://connect.stripe.com/express/oauth/authorize' + '?' + querystring.stringify(parameters)
@@ -28,35 +49,38 @@ router.get('/onboarding/setup', async (req, res) => {
 });
 
 router.get('/onboarding/complete', async (req, res) => {
+   // Check the `state` we got back equals the one we generated before proceeding (to protect from CSRF)
+   if (req.session.state != req.query.state) {
+    return res.redirect('/');
+  }
+
+  const response = await stripe.oauth.token({
+    grant_type: 'authorization_code',
+    code: req.query.code,
+  });
+
+  const connected_account_id = response.stripe_user_id;
+
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+  });
+
+  await doc.loadInfo();
+
+  const sheet = doc.sheetsByIndex[0];
+  const rows = await sheet.getRows();
+  const rowsLength = rows.length;
+
+  for (var i = 0; i < rowsLength; i++) {
+    if (rows[i].FV_User_Email === req.query.state) {
+      rows[i].Stripe_Merchant_ID = connected_account_id;
+      await rows[i].save(); // save changes
+    }
+  }
+
   res.render('onboardingComplete.html');
 });
 
-// router.post('/register', (req, res) => {
-//   const plan = JSON.parse(req.body.plan);
-//   plan.formatted = req.body.plan;
-
-//   res.render('register.html', {
-//     productName: req.body.productName,
-//     plan,
-//   });
-// });
-
-
-// router.post('/handlePayment', async (req, res) => {
-//   const parsedPlan = JSON.parse(req.body.plan);
-
-//   const customerInfo = {
-//     name: req.body.name,
-//     email: req.body.email,
-//     planId: parsedPlan.id,
-//   };
-
-//   const subscription = await STRIPE_API.createCustomerAndSubscription(
-//     req.body.paymentMethodId,
-//     customerInfo,
-//   );
-
-//   return res.json({ subscription });
-// });
 
 module.exports = router;
