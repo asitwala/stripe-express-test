b33a35270a7da470672e181211556218e3727e07
Amy Sitwala <asitwala17@gmail.com>
2020-06-28 19:11:48 -0700

    Hooking up Stripe connect

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 
+var session = require('express-session')
 const app = express();
 const router = require('./router');
 const port = 3000;
@@ -19,6 +20,18 @@ app.use(express.json());
 
 /* To use 'req.body' -- to parse 'application/x-www-form-urlencoded' content-type */
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "sessionSecret2147",
+    resave: false,
+    saveUninitialized: true,
+    duration: 30 * 60 * 1000,
+    activeDuration: 5 * 60 * 1000,
+    maxAge: 30 * 60 * 1000,
+    cookie: { maxAge: 30 * 60 * 1000 }
+  })
+);
 
 /* Set up router */
 app.use('/', router);
--- router/index.js
+++ router/index.js
@@ -1,9 +1,34 @@
+require('dotenv').config();
 const express = require('express');
+const querystring = require('querystring');
 // const STRIPE_API = require('../api/stripe-functions');
 var router = express.Router();
 
 router.get('/', async (req, res) => {
   res.render('onboarding.html');
+});
+
+router.get('/onboarding/setup', async (req, res) => {
+  // Test client ID: ca_HYI7b0d1kneMHXpkggxRtkwrX1f5EdlB
+
+  // Reference: https://github.com/stripe/stripe-connect-rocketrides/blob/master/server/routes/pilots/stripe.js
+  // Generate a random string as `state` to protect from CSRF and include it in the session
+  req.session.state = Math.random()
+    .toString(36)
+    .slice(2);
+
+  let parameters = {
+    redirect_uri: 'https://connect.stripe.com/connect/default/oauth/test',
+    client_id: process.env.STRIPE_CLIENT_ID,
+    state: req.session.state,
+  };
+  res.redirect(
+    'https://connect.stripe.com/express/oauth/authorize' + '?' + querystring.stringify(parameters)
+  );
+});
+
+router.get('/onboarding/complete', async (req, res) => {
+  res.render('onboardingComplete.html');
 });
 
 // router.post('/register', (req, res) => {
